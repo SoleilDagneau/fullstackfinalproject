bb396dbdff7c111c9cf727e11209983215110d3c
soleildagneau <sdagneau@my.bcit.ca>
2023-03-26 19:12:02 -0700

    next auth and prisma set up

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,18 @@
 import '@/styles/globals.css'
+import '../styles/globals.css'
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import { SessionProvider } from "next-auth/react"
+
+
+function MyApp({ Component, pageProps: { session, ...pageProps }, }) {
+  return (
+    <>
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
+    </>
+  )
 }
+
+export default MyApp
+
